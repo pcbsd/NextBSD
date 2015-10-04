c770cd4bec8867725967c91f90abe9b079dd5e61
Jordan Hubbard <jkh@ixsystems.com>
2015-10-04 13:14:00 -0700

    I was wroooooong
    Revert "We don't appear to have a "libnetbsd" and the tests compile without it,"
    
    This reverts commit f01222bd22eaded63775d7eb53e23cb5565e9527.

--- share/mk/netbsd-tests.test.mk
+++ share/mk/netbsd-tests.test.mk
@@ -24,8 +24,8 @@ LIBNETBSD_OBJDIR=	${OBJTOP}/lib/libnetbsd
 CFLAGS.$t+=	-I${LIBNETBSD_SRCDIR} -I${SRCTOP}/contrib/netbsd-tests
 LDFLAGS.$t+=	-L${LIBNETBSD_OBJDIR}
 
-#DPADD.$t+=	${LIBNETBSD}
-#LDADD.$t+=	-lnetbsd
+DPADD.$t+=	${LIBNETBSD}
+LDADD.$t+=	-lnetbsd
 
 SRCS.$t?=	${t:C/^/t_/:C/_test$//g}.c
 .endfor
