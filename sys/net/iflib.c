61d546ab71f628d17a07a2bf579efac19eb90d0b
Matthew Macy <mmacy@nextbsd.org>
2015-10-14 15:51:10 -0700

    ensure that vectors and nqsets is valid even for the legacy case

--- sys/net/iflib.c
+++ sys/net/iflib.c
@@ -2891,6 +2891,8 @@ iflib_device_register(device_t dev, void *sc, if_shared_ctx_t sctx, if_ctx_t *ct
 	} else if (scctx->isc_msix_bar != 0)
 		msix = iflib_msix_init(ctx);
 	else {
+		scctx->isc_vectors = 1;
+		scctx->isc_nqsets = 1;
 		scctx->isc_intr = IFLIB_INTR_LEGACY;
 		msix = 0;
 	}
